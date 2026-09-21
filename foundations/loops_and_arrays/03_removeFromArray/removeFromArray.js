aaa943121f885afe65950a5197f05b70480121c3
Euuiiii <emonadhikari321@gmail.com>
2026-09-21 22:12:45 +0545

    Soln of 01 - 04 of loops_and_array of foundations

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(nums, rmv) {
+    return nums.filter(num => num !== rmv);
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(start < 0 || end < 0) return "ERROR";
 
+    if(start > end) [start, end] = [end, start];
+
+    /*
+        const temp = start;
+        start = end;
+        end = temp; 
+    */
+
+
+    for(let i = start + 1; i <= end; i++){
+      start += i;
+    }
+    return start;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
