0662cab612fee985042b893a5eae5adbe23270a1
Euuiiii <emonadhikari321@gmail.com>
2026-09-22 06:15:59 +0545

    Soln of 05 and 06  of loops_and_array of foundations

--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let f = fahrenheit;
+
+  return ((f - 32) * 5/9).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let c = celsius;
+
+  return ((100 * 9/5) + 32).toFixed(1);
 };
 
 // Do not edit below this line
